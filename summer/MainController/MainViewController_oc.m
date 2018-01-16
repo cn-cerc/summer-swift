be635434baca03fb5f4c8e55b6be2fcde0f281b2
stephen <1402020165@qq.com>
2018-01-16 10:11:16 +0800

    popMenu的iPhoneX适配

--- summer/MainController/MainViewController_oc.m
+++ summer/MainController/MainViewController_oc.m
@@ -221,8 +221,7 @@
     }
 }
 
-#pragma mark - WKScriptMessageHandler
-//js交互回调
+#pragma mark - WKScriptMessageHandler js交互回调
 - (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message {
     NSDictionary *dict = (NSDictionary *)message.body;
     NSString *type = dict[@"classCode"];
@@ -366,7 +365,7 @@
 #pragma mark - 标题按钮
 - (void)titleClick {
     NSArray *dataDict = @[@{@"icon" : @"", @"title" : @"转到首页"}];
-    _popMenu = [[SwiftPopMenu alloc]initWithFrame:CGRectMake(kScreen_width/2-75, 51, 150, dataDict.count * 40) arrowMargin:17];
+    _popMenu = [[SwiftPopMenu alloc]initWithFrame:CGRectMake(kScreen_width/2-75, 51 + iPhoneX ?20 :0, 150, dataDict.count * 40) arrowMargin:17];
     //数据
     _popMenu.popData = dataDict;
     //点击菜单的回调
